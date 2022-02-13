49d5c1d183ee74f597276e427a41f1940a2fa5f8
sabine-xuaitsep <sabinepestiaux@gmail.com>
2022-02-13 03:30:49 +0100

    Relationships between models added

--- app/Models/Work.php
+++ app/Models/Work.php
@@ -2,10 +2,20 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Tag;
+use App\Models\Client;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Work extends Model
 {
     use HasFactory;
+
+    public function client() {
+        return $this->belongsTo(Client::class);
+    }
+
+    public function tags() {
+        return $this->belongsToMany(Tag::class, 'works_has_tags');
+    }
 }
